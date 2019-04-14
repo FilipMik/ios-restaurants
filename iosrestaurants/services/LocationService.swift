4c8024e0a665bc54e910bf82fe6fab8112d6b652
FilipMik <filip.mik@thefuntasty.com>
2019-04-14 10:25:58 +0200

    Set correct author name

--- iosrestaurants/services/LocationService.swift
+++ iosrestaurants/services/LocationService.swift
@@ -2,7 +2,7 @@
 //  LocationService.swift
 //  iosrestaurants
 //
-//  Created by Tatiana Kminiakova on 2.4.19.
+//  Created by Filip Mik on 2.4.19.
 //  Copyright © 2019 pv239. All rights reserved.
 //
 
